5bbdccae5c855a1ff91d5a946cc065276f771be1
Bakunzi <bakunzifiston@gmail.com>
2026-09-03 11:25:48 +0200

    Last

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -8,6 +8,9 @@ class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
-        $this->call(TenantDemoSeeder::class);
+        $this->call([
+            TenantDemoSeeder::class,
+            MarketplaceSeeder::class,
+        ]);
     }
 }
